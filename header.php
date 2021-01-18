8c83e141c1cb9569ed5200c2859f8f1de13a332c
Michael Speranza <mikesperanza34@gmail.com>
2021-01-18 13:54:43 -0500

    Update header.php

--- header.php
+++ header.php
@@ -1,3 +1,11 @@
+<?php
+// Initialize the session
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+	header("location: login.php");
+    exit();
+}
+?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="home.php">Weight Loss App</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -6,7 +14,7 @@
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
+      <li class="nav-item">
         <a class="nav-link" href="calcalc.php">Calorie Calculator</a>
       </li>
       <li class="nav-item">
@@ -24,12 +32,3 @@
     </ul>
   </div>
 </nav>
-<?php
-// Initialize the session
-session_start();
- 
-// Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php");
-    exit;
-} ?>
